a3ab1e7ed07e91b19edaa9978b08d58d479b8421
Tofik <smilokiller@mail.ru>
2020-07-17 10:13:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// first minitask
+
+const myNumb = 15;
+const secondNumb = 20;
+let summ = (myNumb + secondNumb);
+console.log(summ);
+
+// second minitask
+const p = 3.14;
+const r = 10;
+let result = Math.pow(r, 2) * p;
+console.log(result);
+
+// third minitask
+const a = "Tofik"
+const b = 22;
+const c = "cars";
+console.log(
+    `Здраствуйте, меня зовут ${a}, мне ${b} и мне нравятся ${c} `
+);
---
+++ module1/index.js
@@ -0,0 +1,116 @@
+// 1 task
+const name = 'Генератор защитного поля';
+let price = 1000;
+console.log(
+    `'Выбран «${name}», цена за штуку ${price} кредитов`
+);
+price = 2000;
+
+console.log(
+    `'Выбран «${name}», цена за штуку ${price} кредитов`
+);
+// 2 task
+const total = 100;
+const ordered = 30;
+
+if (ordered <= total) {
+    console.log("Заказ оформлен, с вами свяжется менеджер");
+
+} else {
+    console.log("На складе недостаточно твоаров!");
+}
+
+//3 task
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+message = prompt('Хотите залогинится?');
+console.log(message);
+if (message == ADMIN_PASSWORD) {
+    console.log("Добро пожаловать!");
+
+} else if (message == null) {
+    console.log("Отменено пользователем!");
+} else {
+    console.log("'Доступ запрещен, неверный пароль!");
+}
+
+// 4 task
+
+let credits = 23580;
+const pricePerDroid = 3000;
+let count;
+count = prompt('Сколько дронов хотите купить?');
+console.log(count);
+if (count == null) {
+    console.log("Отменено пользователем!");
+} else {
+
+    if (isNaN(count) == false) {
+        credits = credits - pricePerDroid * count;
+        if (credits > 0) {
+            console.log(`Вы купили ${count} дроидов, на счету осталось ${credits} кредитов.`);
+        } else {
+            console.log("Недостаточно средств на счету!");
+        }
+    } else {
+        console.log("Нужно ввести число");
+    }
+}
+
+// 5 task
+
+let country;
+country = prompt("Введите страну").toLowerCase();
+console.log(country);
+switch (country) {
+    case 'китай':
+        let countryPrice = 100;
+        console.log(`Доставка в ${country} будет стоить ${countryPrice} кредитов.`);
+        break;
+
+    case 'чили':
+        countryPrice = 250;
+        console.log(`Доставка в ${country} будет стоить ${countryPrice} кредитов.`);
+        break;
+
+    case 'австралия':
+        countryPrice = 170;
+        console.log(`Доставка в ${country} будет стоить ${countryPrice} кредитов.`);
+        break;
+
+    case 'индия':
+        countryPrice = 80;
+        console.log(`Доставка в ${country} будет стоить ${countryPrice} кредитов.`);
+        break;
+
+    case 'ямайка':
+        countryPrice = 120;
+        console.log(`Доставка в ${country} будет стоить ${countryPrice} кредитов.`);
+        break;
+
+    default:
+        console.log(`В вашей стране доставка не доступна`);
+
+}
+
+// 6 task
+
+let input;
+input = prompt("Введите число");
+let totalNumb = 0;
+// if (isNaN(input) == fasle) {
+//     totalNumb = totalNumb + input
+// } else if (input == null) {
+//     console.log(`Общая сумма чисел равна ${totalNumb}`);
+
+// } else {
+
+// }
+do {
+    input = prompt("Введите число");
+    let num = Number(input);
+    totalNumb = totalNumb + num;
+}
+while (input !== null)
+console.log(`Общая сумма чисел равна ${totalNumb}`);
